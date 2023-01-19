47ce9ff0212b6ef994c8fe7defb0eae1565a3ff0
Abdullah <abdullahalasbahia@gmail.com>
2023-01-19 04:42:29 +0200

    stable version choose one answer...

--- src/Answer.js
+++ src/Answer.js
@@ -5,16 +5,6 @@ function Answer(props){
     const [allAnswers, setAllAnswers] = React.useState(props.answersArray.map(answer => {
         return ({text: answer, isHold: false, id: Math.random()})}))
 
-        console.log(props.answersArray)
-
-    let choise = ['','','','',''];
-    function mapOverAnswers(){
-        // console.log("hello")
-        choise = allAnswers;
-        console.log(choise)
-    }
-    // mapOverAnswers()
-
     function chooseAnswer(id){
         setAllAnswers(prevAnswers => {
             return (prevAnswers.map(answer => {
--- src/App.js
+++ src/App.js
@@ -5,32 +5,10 @@ import shuffle from "./shuffle"
 import PlayAgainButton from "./PlayAgainButton";
 
 function App(){
-    
-
-    // {
-    //     "category": "Science: Computers",
-    //     "type": "multiple",
-    //     "difficulty": "medium",
-    //     "question": "What was the first Android version specifically optimized for tablets?",
-    //     "correct_answer": "Honeycomb",
-    //     "incorrect_answers": [
-    //         "Eclair",
-    //         "Froyo",
-    //         "Marshmellow"
-    //     ]
-    // }
 
     const [questions, setQuestions] = React.useState();
-    // const [startingQuiz, setStartingQuiz] = React.useState(true);
     
-    console.log(questions)
-
-    let answersPlaceHolder = questions.map(q => "");
-    console.log(answersPlaceHolder)
-    function choosenAnswersInChild(){
-
-    }
-    
+   
     React.useEffect(function() {
         fetch("https://opentdb.com/api.php?amount=5&category=18")
         .then(res => res.json())
@@ -46,8 +24,6 @@ function App(){
     return (
         <main>
         <div className="quiz-welcome-screen">
-            <pre>{JSON.stringify(questions, null, 4)}</pre>
-            {/* If there is Any (questions) && Do what you have to do */}
             {questions && questions.map(function(result) { // MAP MAP MAP <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
                 const answersArray = []; // empty array to but all the answers in
                 const correctAnswer = result.correct_answer; // correct answers in a constant
